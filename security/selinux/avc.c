4d8e549b25762d6e3ca1da2ede9526372cce98c9
Iam-nishan <real.thelinuxlore@gmail.com>
2026-03-19 19:57:56 +0530

    Update avc.c

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -167,9 +167,7 @@ static void avc_dump_av(struct audit_buffer *ab, u16 tclass, u32 av)
 }
 #ifdef CONFIG_KSU_SUSFS
 extern u32 susfs_ksu_sid;
-extern u32 susfs_priv_app_sid;
-bool susfs_is_avc_log_spoofing_enabled = false;
-EXPORT_SYMBOL(susfs_is_avc_log_spoofing_enabled);
+extern u32 susfs_priv_app_sid
 #endif
 
 /**
