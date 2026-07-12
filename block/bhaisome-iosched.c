92af01e20eeaeb680799fc31ea5e261e174e3e69
Iam-nishan <real.thelinuxlore@gmail.com>
2026-07-12 13:04:40 +0530

    Uhh maybe this fix

--- block/bhaisome-iosched.c
+++ block/bhaisome-iosched.c
@@ -1,10 +1,9 @@
 /*
- * Bhaisome IO scheduler - blk-mq version
- * Copyright (C) 2025 Bhaisome
- * Derived from Anxiety IO scheduler by Draco (Tyler Nijmeh)
- *
- * blk-mq version for Linux 4.19+ / MTK Helio G25 / eMMC 5.1
+ * Bhaisome IO scheduler - Improved blk-mq version
+ * Copyright (C) 2025 Nishan_Najmal (Bhaisome)
+ * Derived from Anxiety + stability ideas from Deadline
  */
+
 #include <linux/blkdev.h>
 #include <linux/elevator.h>
 #include <linux/bio.h>
@@ -13,6 +12,7 @@
 #include <linux/init.h>
 #include <linux/list.h>
 
+/* default tunable */
 static const int max_writes_starved = 16;
 
 struct bhaisome_data {
@@ -25,11 +25,13 @@ static __always_inline struct request *bhaisome_choose_request(struct bhaisome_d
 {
 	bool starved = mdata->writes_starved > mdata->max_writes_starved;
 
+	/* Prioritize reads unless writes are starved */
 	if (!starved && !list_empty(&mdata->queue[READ])) {
 		mdata->writes_starved++;
 		return list_entry_rq(mdata->queue[READ].next);
 	}
 
+	/* Serve writes */
 	if (!list_empty(&mdata->queue[WRITE])) {
 		mdata->writes_starved = 0;
 		return list_entry_rq(mdata->queue[WRITE].next);
@@ -39,49 +41,60 @@ static __always_inline struct request *bhaisome_choose_request(struct bhaisome_d
 	return NULL;
 }
 
+static int bhaisome_merge(struct request_queue *q, struct request **req, struct bio *bio)
+{
+	return ELEVATOR_NO_MERGE;   /* Simple FIFO for now */
+}
+
+static void bhaisome_merged_requests(struct request_queue *q, struct request *req,
+				     struct request *next)
+{
+	if (!list_empty(&next->queuelist))
+		list_del_init(&next->queuelist);
+}
+
 static int bhaisome_dispatch(struct request_queue *q, int force)
 {
-	struct bhaisome_data *mdata = q->elevator->elevator_data;
+	struct bhaisome_data *mdata;
 	struct request *rq;
 
+	if (!q->elevator || !q->elevator->elevator_data)
+		return 0;
+
+	mdata = q->elevator->elevator_data;
 	rq = bhaisome_choose_request(mdata);
+
 	if (!rq)
 		return 0;
 
 	list_del_init(&rq->queuelist);
-	elv_dispatch_add_tail(rq->q, rq);
+	elv_dispatch_add_tail(q, rq);
 
 	return 1;
 }
 
 static void bhaisome_add_request(struct request_queue *q, struct request *rq)
 {
-	struct bhaisome_data *mdata = q->elevator->elevator_data;
+	struct bhaisome_data *mdata;
 	const int dir = rq_data_dir(rq);
 
+	if (!q->elevator || !q->elevator->elevator_data) {
+		elv_dispatch_add_tail(q, rq);
+		return;
+	}
+
+	mdata = q->elevator->elevator_data;
 	list_add_tail(&rq->queuelist, &mdata->queue[dir]);
 }
 
 static struct request *bhaisome_former_request(struct request_queue *q, struct request *rq)
 {
-	struct bhaisome_data *mdata = q->elevator->elevator_data;
-	const int dir = rq_data_dir(rq);
-
-	if (rq->queuelist.prev == &mdata->queue[dir])
-		return NULL;
-
-	return list_prev_entry(rq, queuelist);
+	return NULL;   /* Not needed for simple FIFO */
 }
 
 static struct request *bhaisome_latter_request(struct request_queue *q, struct request *rq)
 {
-	struct bhaisome_data *mdata = q->elevator->elevator_data;
-	const int dir = rq_data_dir(rq);
-
-	if (rq->queuelist.next == &mdata->queue[dir])
-		return NULL;
-
-	return list_next_entry(rq, queuelist);
+	return NULL;
 }
 
 static int bhaisome_init_queue(struct request_queue *q, struct elevator_type *elv)
@@ -98,6 +111,7 @@ static int bhaisome_init_queue(struct request_queue *q, struct elevator_type *el
 		kobject_put(&eq->kobj);
 		return -ENOMEM;
 	}
+
 	eq->elevator_data = data;
 
 	INIT_LIST_HEAD(&data->queue[READ]);
@@ -117,6 +131,7 @@ static void bhaisome_exit_queue(struct elevator_queue *eq)
 	kfree(eq->elevator_data);
 }
 
+/* Sysfs tunable */
 static ssize_t bhaisome_max_writes_starved_show(struct elevator_queue *e, char *page)
 {
 	struct bhaisome_data *ad = e->elevator_data;
@@ -126,13 +141,8 @@ static ssize_t bhaisome_max_writes_starved_show(struct elevator_queue *e, char *
 static ssize_t bhaisome_max_writes_starved_store(struct elevator_queue *e, const char *page, size_t count)
 {
 	struct bhaisome_data *ad = e->elevator_data;
-	int ret;
-
-	ret = kstrtouint(page, 0, &ad->max_writes_starved);
-	if (ret < 0)
-		return ret;
-
-	return count;
+	int ret = kstrtouint(page, 0, &ad->max_writes_starved);
+	return ret < 0 ? ret : count;
 }
 
 static struct elv_fs_entry bhaisome_attrs[] = {
@@ -143,6 +153,8 @@ static struct elv_fs_entry bhaisome_attrs[] = {
 static struct elevator_type elevator_bhaisome = {
 	.ops = {
 		.sq = {
+			.elevator_merge_fn		= bhaisome_merge,
+			.elevator_merge_req_fn		= bhaisome_merged_requests,
 			.elevator_dispatch_fn		= bhaisome_dispatch,
 			.elevator_add_req_fn		= bhaisome_add_request,
 			.elevator_former_req_fn		= bhaisome_former_request,
@@ -172,4 +184,4 @@ module_exit(bhaisome_exit);
 
 MODULE_AUTHOR("Nishan_Najmal");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Bhaisome IO scheduler - blk-mq");
+MODULE_DESCRIPTION("Bhaisome IO scheduler");
