41a8e54c32e7d5cf2bbeeea7b1b8b67b9aa26ba3
Iam-nishan <real.thelinuxlore@gmail.com>
2026-08-02 17:09:06 +0530

    Revert

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -49,14 +49,7 @@ static inline void set_max_mapnr(unsigned long limit)
 static inline void set_max_mapnr(unsigned long limit) { }
 #endif
 
-extern atomic_long_t _totalram_pages;
-
-static inline unsigned long totalram_pages(void)
-{
-	return (unsigned long)atomic_long_read(&_totalram_pages);
-}
-#define totalram_pages totalram_pages()
-
+extern unsigned long totalram_pages;
 extern void * high_memory;
 extern int page_cluster;
 
