1c2b77e6dcd9780d3dbf54329d7aba7a4a6a027b
Awmid <ahmaddbl86@gmail.com>
2026-09-20 17:25:17 +0300

    restore vendor code dropped by reverting to wrong originals (memory.c userfaultfd, selinux hooks, path_umount, sched.h, fd.c)

--- fs/namespace.c
+++ fs/namespace.c
@@ -1119,9 +1119,7 @@ vfs_kern_mount(struct file_system_type *type, int flags, const char *name, void 
 		}
 	}
 #endif // #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-
-	mnt = alloc_vfsmnt(name);
-
+    mnt = alloc_vfsmnt(name);
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 bypass_orig_flow:
 #endif // #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
@@ -1590,7 +1588,7 @@ static void namespace_unlock(void)
 	if (likely(hlist_empty(&head)))
 		return;
 
-	synchronize_rcu();
+	synchronize_rcu_expedited();
 
 	group_pin_kill(&head);
 }
@@ -1921,6 +1919,37 @@ SYSCALL_DEFINE1(oldumount, char __user *, name)
 }
 
 #endif
+
+static int can_umount(const struct path *path, int flags)
+ {
+	 struct mount *mnt = real_mount(path->mnt);
+	 if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
+		 return -EINVAL;
+	 if (!may_mount())
+		 return -EPERM;
+	 if (path->dentry != path->mnt->mnt_root)
+		 return -EINVAL;
+	 if (!check_mnt(mnt))
+		 return -EINVAL;
+	 if (mnt->mnt.mnt_flags & MNT_LOCKED)
+		 return -EINVAL;
+	 if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
+		 return -EPERM;
+	 return 0;
+ }
+
+int path_umount(struct path *path, int flags)
+ {
+	 struct mount *mnt = real_mount(path->mnt);
+	 int ret;
+	 ret = can_umount(path, flags);
+	 if (!ret)
+		 ret = do_umount(mnt, flags);
+	 dput(path->dentry);
+	 mntput_no_expire(mnt);
+	 return ret;
+ }
+ EXPORT_SYMBOL_GPL(path_umount);
 
 static bool is_mnt_ns_file(struct dentry *dentry)
 {
